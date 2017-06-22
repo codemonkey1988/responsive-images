db41015e91b6af2292ae3d0737a9f31a975cbe1f
Tim Schreiner <schreiner@arndtteunissen.de>
2017-06-22 23:07:29 +0200

    Raise version to 1.0.3

--- ext_emconf.php
+++ ext_emconf.php
@@ -17,5 +17,5 @@ $EM_CONF[$_EXTKEY] = array(
     'author'           => 'Tim Schreiner',
     'author_email'     => 'schreiner.tim@gmail.com',
     'author_company'   => '',
-    'version'          => '1.0.3-dev'
+    'version'          => '1.0.3'
 );
