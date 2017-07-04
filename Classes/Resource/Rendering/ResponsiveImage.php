48ab334169c1ad0f0547d82144bc50c4baef8fd5
Joshua Westerheide <dev@jdoubleu.de>
2017-07-04 08:53:11 +0200

    Add key for post scaling processed images

--- Classes/Resource/Rendering/ResponsiveImage.php
+++ Classes/Resource/Rendering/ResponsiveImage.php
@@ -32,6 +32,9 @@ class ResponsiveImage implements FileRendererInterface
 {
     const DEFAULT_IMAGE_VARIANT_KEY = 'default';
     const REGISTER_IMAGE_VARIANT_KEY = 'IMAGE_VARIANT_KEY';
+    const REGISTER_IMAGE_RELATVE_WIDTH_KEY = 'IMAGE_RELATIVE_WIDTH_KEY';
+    const OPTIONS_IMAGE_RELATVE_WIDTH_KEY = 'relativeScalingWidth';
+
     /**
      * @var \TYPO3\CMS\Extbase\Object\ObjectManagerInterface
      */
@@ -77,6 +80,12 @@ class ResponsiveImage implements FileRendererInterface
     public function render(FileInterface $file, $width, $height, array $options = [], $usedPathsRelativeToCurrentScript = false)
     {
         $this->objectManager = GeneralUtility::makeInstance(ObjectManager::class);
+
+        if (!array_key_exists(self::OPTIONS_IMAGE_RELATVE_WIDTH_KEY, $options)
+            && isset($GLOBALS['TSFE']->register[self::REGISTER_IMAGE_RELATVE_WIDTH_KEY])
+        ) {
+            $options[self::OPTIONS_IMAGE_RELATVE_WIDTH_KEY] = $GLOBALS['TSFE']->register[self::REGISTER_IMAGE_RELATVE_WIDTH_KEY];
+        }
 
         // Check if a responsive image tag should be rendered. If not, just return the normal image tag.
         if (isset($options['disablePictureTag']) && $options['disablePictureTag'] == true) {
