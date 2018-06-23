a0d27da8f8ee91a8fb6bf588ee07bf1f4811b54e
Tim Schreiner <schreiner@arndtteunissen.de>
2018-06-23 07:42:51 +0200

    Remove backwards compatility for 1.0.0

--- Migration/Code/ClassAliasMap.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    'Codemonkey1988\\ResponsiveImages\\Resource\\Rendering\\PictureImageVariant' => \Codemonkey1988\ResponsiveImages\Resource\Service\PictureImageVariant::class,
-    'Codemonkey1988\\ResponsiveImages\\Resource\\Rendering\\PictureVariantsRegistry' => \Codemonkey1988\ResponsiveImages\Resource\Service\PictureVariantsRegistry::class,
-];
