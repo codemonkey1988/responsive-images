ee042b92827648d1a990d89791e79c7345faf697
Tim Schreiner <gitlab@tim-schreiner.de>
2020-02-26 20:39:01 +0100

    [TASK] Set version to 2.3.0-dev

--- ext_emconf.php
+++ ext_emconf.php
@@ -17,5 +17,5 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Tim Schreiner',
     'author_email' => 'schreiner.tim@gmail.com',
     'author_company' => '',
-    'version' => '2.2.4',
+    'version' => '2.3.0-dev',
 ];
