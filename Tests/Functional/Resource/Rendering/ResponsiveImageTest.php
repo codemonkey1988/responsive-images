50f282c1ac8fa842b7079dfabbee688ca7577e29
Tim Schreiner <gitlab@tim-schreiner.de>
2020-02-27 02:59:20 +0100

    [TASK] Adjust functional tests to use frontend request instead of mocking objects

--- Tests/Functional/Resource/Rendering/ResponsiveImageTest.php
+++ Tests/Functional/Resource/Rendering/ResponsiveImageTest.php
@@ -13,17 +13,8 @@ namespace Codemonkey1988\ResponsiveImages\Tests\Functional\Resource\Rendering;
  *
  */
 
-use Codemonkey1988\ResponsiveImages\Resource\Rendering\ResponsiveImageRenderer;
-use Codemonkey1988\ResponsiveImages\Resource\Service\PictureVariantsRegistry;
 use Nimut\TestingFramework\TestCase\FunctionalTestCase;
-use TYPO3\CMS\Core\Authentication\BackendUserAuthentication;
-use TYPO3\CMS\Core\Resource\FileRepository;
-use TYPO3\CMS\Core\TypoScript\Parser\TypoScriptParser;
-use TYPO3\CMS\Core\TypoScript\TemplateService;
-use TYPO3\CMS\Core\Utility\ArrayUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
-use TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController;
 
 /**
  * Test class for \Codemonkey1988\ResponsiveImages\Resource\Rendering\ResponsiveImageRenderer
@@ -38,57 +29,24 @@ class ResponsiveImageTest extends FunctionalTestCase
         'typo3conf/ext/responsive_images',
     ];
 
+    protected $typoScriptIncludes = [];
+
     protected function setUp()
     {
         parent::setUp();
 
         $this->importDataSet(__DIR__ . '/../../Fixtures/sys_file_storage.xml');
         $this->importDataSet(__DIR__ . '/../../Fixtures/sys_file.xml');
+        $this->importDataSet(__DIR__ . '/../../Fixtures/pages.xml');
 
-        $typoscriptIncludes = [
+        $this->getDatabaseConnection()->delete('sys_file_processedfile', ['storage' => '1']);
+
+        $this->typoScriptIncludes = [
             GeneralUtility::getFileAbsFileName('EXT:responsive_images/Configuration/TypoScript/setup.typoscript'),
             GeneralUtility::getFileAbsFileName('EXT:responsive_images/Configuration/TypoScript/DefaultConfiguration/setup.typoscript'),
         ];
 
-        $tsfeMock = $this->getMockBuilder(TypoScriptFrontendController::class)
-            ->disableOriginalConstructor()
-            ->getMock();
-
-        $tsfeMock->tmpl = new TemplateService();
-        $tsfeMock->tmpl->setup = [];
-
-        foreach ($typoscriptIncludes as $additionalTypoScript) {
-            $config = GeneralUtility::makeInstance(TypoScriptParser::class);
-            $config->parse(file_get_contents($additionalTypoScript));
-            ArrayUtility::mergeRecursiveWithOverrule(
-                $tsfeMock->tmpl->setup,
-                $config->setup
-            );
-        }
-
-        $contentObjectRenderer = new ContentObjectRenderer($tsfeMock);
-        $contentObjectRenderer->data = [];
-
-        $tsfeMock->cObj = $contentObjectRenderer;
-
-        $GLOBALS['TSFE'] = $tsfeMock;
-        $GLOBALS['BE_USER'] = $this->getMockBuilder(BackendUserAuthentication::class)
-            ->disableOriginalConstructor()
-            ->getMock();
-        $GLOBALS['BE_USER']->expects($this->any())
-            ->method('isAdmin')
-            ->willReturn(true);
-    }
-
-    protected function tearDown()
-    {
-        parent::tearDown();
-
-        unset($GLOBALS['TSFE']);
-        unset($GLOBALS['BE_USER']);
-
-        $registry = PictureVariantsRegistry::getInstance();
-        $registry->removeAllImageVariants();
+        $this->setUpBackendUserFromFixture(1);
     }
 
     /**
@@ -96,11 +54,9 @@ class ResponsiveImageTest extends FunctionalTestCase
      */
     public function generateImageTagForValidJpegImage()
     {
-        $fileRepository = GeneralUtility::makeInstance(FileRepository::class);
-        $file = $fileRepository->findByUid(1);
-
-        $subject = GeneralUtility::makeInstance(ResponsiveImageRenderer::class);
-        $result = trim($subject->render($file, 0, 0), "\n ");
+        $result = $this->getContentFromFrontendRequest(
+            __DIR__ . '/../../Fixtures/config/pictureTag/pageConfig.typoscript'
+        );
 
         $this->assertRegExp('/^<picture>.*<\/picture>$/', $result);
         $this->assertRegExp('/<source media=".*" srcset=".*" \/>/', $result);
@@ -111,11 +67,9 @@ class ResponsiveImageTest extends FunctionalTestCase
      */
     public function generateImageTagForValidJpegImageButDisabledPictureTag()
     {
-        $fileRepository = GeneralUtility::makeInstance(FileRepository::class);
-        $file = $fileRepository->findByUid(1);
-
-        $subject = GeneralUtility::makeInstance(ResponsiveImageRenderer::class);
-        $result = trim($subject->render($file, 0, 0, ['disablePictureTag' => true]), "\n ");
+        $result = $this->getContentFromFrontendRequest(
+            __DIR__ . '/../../Fixtures/config/noPictureTag/pageConfig.typoscript'
+        );
 
         $this->assertRegExp('/^<img src=".*" width="1920" height="1056" alt="" \/>$/', $result);
     }
@@ -123,21 +77,15 @@ class ResponsiveImageTest extends FunctionalTestCase
     /**
      * @test
      */
-    public function generatePictureTagForValidJpegImageWithoutImageProcessingDisabledByEnv()
+    public function generatePictureTagForValidJpegImageWithoutImageProcessingDisabledByTypoScript()
     {
-        putenv('RESPONSIVE_IMAGES_PROCESSING=0');
-
-        $fileRepository = GeneralUtility::makeInstance(FileRepository::class);
-        $file = $fileRepository->findByUid(1);
-
-        $subject = GeneralUtility::makeInstance(ResponsiveImageRenderer::class);
-        $result = trim($subject->render($file, 0, 0), "\n ");
-
-        putenv('RESPONSIVE_IMAGES_PROCESSING');
+        $result = $this->getContentFromFrontendRequest(
+            __DIR__ . '/../../Fixtures/config/pictureTagWithoutProcessing/pageConfig.typoscript'
+        );
 
         $imagePaths = [
-            '.Build/bin/typo3conf/ext/responsive_images/Tests/Functional/Fixtures/fileadmin/example.jpg 1x',
-            '.Build/bin/typo3conf/ext/responsive_images/Tests/Functional/Fixtures/fileadmin/example.jpg 2x',
+            '/typo3conf/ext/responsive_images/Tests/Functional/Fixtures/fileadmin/example.jpg 1x',
+            '/typo3conf/ext/responsive_images/Tests/Functional/Fixtures/fileadmin/example.jpg 2x',
         ];
 
         $this->assertRegExp('/^<picture>.*<\/picture>$/', $result);
@@ -147,26 +95,20 @@ class ResponsiveImageTest extends FunctionalTestCase
     }
 
     /**
-     * @test
+     * @param string $additionalTypoScriptIncludes
+     * @return string
      */
-    public function generatePictureTagForValidJpegImageWithoutImageProcessingDisabledByTypoScript()
+    protected function getContentFromFrontendRequest($additionalTypoScriptIncludes)
     {
-        $GLOBALS['TSFE']->tmpl->setup['plugin.']['tx_responsiveimages.']['settings.']['processing'] = '0';
+        $this->setUpFrontendRootPage(1, array_merge(
+            $this->typoScriptIncludes,
+            [
+                $additionalTypoScriptIncludes
+            ]
+        ));
 
-        $fileRepository = GeneralUtility::makeInstance(FileRepository::class);
-        $file = $fileRepository->findByUid(1);
+        $response = $this->getFrontendResponse(1);
 
-        $subject = GeneralUtility::makeInstance(ResponsiveImageRenderer::class);
-        $result = trim($subject->render($file, 0, 0), "\n ");
-
-        $imagePaths = [
-            '.Build/bin/typo3conf/ext/responsive_images/Tests/Functional/Fixtures/fileadmin/example.jpg 1x',
-            '.Build/bin/typo3conf/ext/responsive_images/Tests/Functional/Fixtures/fileadmin/example.jpg 2x',
-        ];
-
-        $this->assertRegExp('/^<picture>.*<\/picture>$/', $result);
-        $this->assertContains('<source media="(max-width: 40em)" srcset="' . implode(',', $imagePaths) . '" />', $result);
-        $this->assertContains('<source media="(min-width: 40.0625em)" srcset="' . implode(',', $imagePaths) . '" />', $result);
-        $this->assertContains('<source media="(min-width: 64.0625em)" srcset="' . $imagePaths[0] . '" />', $result);
+        return trim($response->getContent(), "\n ");
     }
 }
