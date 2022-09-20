54f5dd7f2a8f9d951d03170da480c232974e6924
Tim Schreiner <dev@tim-schreiner.de>
2022-09-20 20:15:53 +0200

    [TASK] Replace TYPO3 support 11.4 with 11.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF['responsive_images'] = [
     'category' => 'fe',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-1.4.99 || 11.4.0-11.4.99',
+            'typo3' => '10.4.0-1.4.99 || 11.4.0-11.5.99',
         ],
         'conflicts' => [],
     ],
