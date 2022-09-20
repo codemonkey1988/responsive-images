960f906cb540b5f044a0e18911524de5ac88a1b4
Tim Schreiner <dev@tim-schreiner.de>
2022-09-20 20:15:50 +0200

    [TASK] Drop support for TYPO3 version 8.7 and 9.5
    In preperation for v11

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'fe',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-10.4.99',
+            'typo3' => '10.4.99',
         ],
         'conflicts' => [],
     ],
